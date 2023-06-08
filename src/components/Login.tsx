d79d73e60d049c7a137ef0f6ceaed9725e15d16b
bryanmpak <bryan.m.pak@gmail.com>
2023-06-08 10:07:09 +0900

    minor ui changes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
       <main className="flex max-w-[500px] h-screen flex-col justify-center items-center bg-bg_main">
         <Navbar />
         <button
-          className="flex w-3/5 gap-1 mx-4 py-2 px-4 text-[#bebebe] bg-bg_secondary justify-between items-center hover:bg-accent_1 hover:text-white rounded-2xl"
+          className="flex w-3/5 gap-1 mx-4 py-2 px-4 text-[#bebebe] bg-bg_secondary justify-between items-center hover:bg-accent_1 hover:text-white rounded"
           onClick={() => signIn("spotify")}
         >
           <Image src={"/spot.svg"} alt="logo" width={36} height={36} />
--- src/components/PromptDesc.tsx
+++ src/components/PromptDesc.tsx
@@ -2,32 +2,36 @@ import React from "react"
 
 function PromptDesc() {
   return (
-    <div className="gap-2 text-white">
-      <p className="mb-2">Create a playlist using the following attributes:</p>
-      {/* Tone/Genre/Occasion/Setting/Featuring Wildcard */}
-      <div className="infoText">
-        <h2 className=" underline decoration-accent_1">tone:</h2>
-        <p className="text-sm font-light">
-          Bright, Energetic, Meloncholy, Pensive, etc
+    <div className=" flex flex-col justify-center items-center flex-1 gap-2 text-white">
+      <div>
+        <p className="mb-2">
+          Create a playlist using the following attributes:
         </p>
-      </div>
-      <div className="infoText">
-        <h2 className=" underline decoration-accent_1">genre:</h2>
-        <p className="text-sm font-light">
-          Hip-hop, R&B, Soul, Pop, K-Pop, etc
-        </p>
-      </div>
-      <div className="infoText">
-        <h2 className=" underline decoration-accent_1">occasion:</h2>
-        <p className="text-sm font-light">
-          Cruising, Working, Working out, etc{" "}
-        </p>
-      </div>
-      <div className="infoText">
-        <h2 className=" underline decoration-accent_1">setting:</h2>
-        <p className="text-sm font-light">
-          At the desk, In the car, In the gym, etc{" "}
-        </p>
+        {/* Tone/Genre/Occasion/Setting/Featuring Wildcard */}
+        <div className="infoText">
+          <h2 className=" underline decoration-accent_1">tone:</h2>
+          <p className="text-sm font-light">
+            Bright, Energetic, Meloncholy, Pensive, etc
+          </p>
+        </div>
+        <div className="infoText">
+          <h2 className=" underline decoration-accent_1">genre:</h2>
+          <p className="text-sm font-light">
+            Hip-hop, R&B, Soul, Pop, Emo-Rock, etc
+          </p>
+        </div>
+        <div className="infoText">
+          <h2 className=" underline decoration-accent_1">occasion:</h2>
+          <p className="text-sm font-light">
+            Cruising, Working, Working out, Chilling, etc
+          </p>
+        </div>
+        <div className="infoText">
+          <h2 className=" underline decoration-accent_1">setting:</h2>
+          <p className="text-sm font-light">
+            At the desk, In the car, In the gym, etc
+          </p>
+        </div>
       </div>
     </div>
   )
